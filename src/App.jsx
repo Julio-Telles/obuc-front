81b08f081c090f0baf19303b957132b264c487f7
Julio-Telles <julio.telles_df@yahoo.com>
2024-11-01 21:00:13 -0300

    commit inicial: usando Vite e comentando a chamada na API pra entender o layout do front

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import Home from "./pages/Home/Home";
+function App() {
+  return (
+    <div id="app-wrapper">
+      {/*
+      */}
+      <Home />
+    </div>
+  )
+}
+
+export default App
